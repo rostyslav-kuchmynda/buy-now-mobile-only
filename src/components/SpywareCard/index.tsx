15f6e009c709efb3804b5e5ea075b09de39cb613
Rostyslav Kuchmynda <rostyk2994@gmail.com>
2023-07-15 10:33:07 +0300

    playsInline attr for mibile support in video tag

--- src/components/SpywareCard/index.tsx
+++ src/components/SpywareCard/index.tsx
@@ -14,7 +14,7 @@ const CAPABILITIES = [
 export const SpywareCard: React.FC = () => (
   <section className={classes.spywareCardWrap}>
     <div className={classes.videoWrap}>
-      <video src={DogVideo} autoPlay loop muted />
+      <video src={DogVideo} autoPlay loop muted playsInline />
     </div>
     <h1 className={classes.spywareCardWrapTitle}>The anti-spy Clario app can:</h1>
     <BulletPointList list={CAPABILITIES} defaultIcon />
